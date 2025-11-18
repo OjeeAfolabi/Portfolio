ee9caa8b8f4ee8d74f01fb0a3d404b68354e239a
OjeeAfolabi <afolabiojee@gmail.com>
2025-11-18 16:38:32 +0100

    more about

--- my-portfolio/src/components/pages/About.jsx
+++ my-portfolio/src/components/pages/About.jsx
@@ -19,7 +19,7 @@ const About = () => {
     "Figma",
     "Canva",
   ];
-    const others = ["Git", "GitHub", "REST APIs", "Agile Methodologies", "JWT", "OAuth"];
+    const others = ["Git", "GitHub", "REST APIs", "JWT", "OAuth"];
 
   return (
     <section
@@ -95,7 +95,7 @@ const About = () => {
                 Security & Others
               </h3>
               <div className="flex flex-wrap gap-2">
-                {backendSkills.map((tech, key) => (
+                {others.map((tech, key) => (
                   <span
                     className="bg-blue-500/10 text-blue-500 py-1 px-3 rounded-full text-sm hover:bg-blue-500/20 hover:shadow-[0_2px_8px_rgba(59,130,2246,0.2)] transition"
                     key={key}
